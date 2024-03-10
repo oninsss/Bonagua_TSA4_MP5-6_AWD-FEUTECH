502c091ec5d54d8427351a46fdf8baf262233e1f
oninsss <antoninobonagua993@gmail.com>
2024-03-10 16:09:03 +0800

    FIX : All issues with the script.js file have been resolved ```

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -24,6 +24,7 @@ const card4BowlPos = -280;
 // if currBowl = 0, then the food bowl is at the first card
 currBowl = 1;
 gotoBowl = 1;
+pastBowl = 4;
 transiLength = 0;
 
 const card1Pos = card1.getBoundingClientRect();
@@ -31,7 +32,18 @@ const card2Pos = card2.getBoundingClientRect();
 const card3Pos = card3.getBoundingClientRect();
 const card4Pos = card4.getBoundingClientRect();
 
-console.log(card1Pos);
+function mainMenuCloser(){
+    foodDescription1.style.opacity = 0;
+    foodDescription1.style.zIndex = -1;
+    foodDescription2.style.opacity = 0;
+    foodDescription2.style.zIndex = -1;
+    foodDescription3.style.opacity = 0;
+    foodDescription3.style.zIndex = -1;
+    foodDescription4.style.opacity = 0;
+    foodDescription4.style.zIndex = -1;
+
+}
+
 
 function fadeOut() {
     foodName.style.opacity = 0;
@@ -44,30 +56,62 @@ function fadeIn() {
 function prevCard() {
     if (currBowl == 1) {
         card4.dispatchEvent(new Event('mouseover'));
+        card4.style.backgroundColor = "white";
+        
     }
     else if (currBowl == 2) {
         card1.dispatchEvent(new Event('mouseover'));
+        card1.style.backgroundColor = "white";
     }
     else if (currBowl == 3) {
         card2.dispatchEvent(new Event('mouseover'));
+        card2.style.backgroundColor = "white";
     }
     else if (currBowl == 4) {
         card3.dispatchEvent(new Event('mouseover'));
+        card3.style.backgroundColor = "white";
+    }
+
+    if (pastBowl == 1) {
+        card1.style.backgroundColor = "transparent";
+    } else if (pastBowl == 2) {
+        card2.style.backgroundColor = "transparent";
+    } else if (pastBowl == 3) {
+        card3.style.backgroundColor = "transparent";
+    } else if (pastBowl == 4) {
+        card4.style.backgroundColor = "transparent";
     }
 }
 
 function nextCard(){
+    
+
     if (currBowl == 1) {
         card2.dispatchEvent(new Event('mouseover'));
+        card2.style.backgroundColor = "white";
+        
     }
     else if (currBowl == 2) {
         card3.dispatchEvent(new Event('mouseover'));
+        card3.style.backgroundColor = "white";
     }
     else if (currBowl == 3) {
         card4.dispatchEvent(new Event('mouseover'));
+        card4.style.backgroundColor = "white";
     }
     else if (currBowl == 4) {
         card1.dispatchEvent(new Event('mouseover'));
+        card1.style.backgroundColor = "white";
+    }
+
+    if (pastBowl == 1) {
+        card1.style.backgroundColor = "transparent";
+    } else if (pastBowl == 2) {
+        card2.style.backgroundColor = "transparent";
+    } else if (pastBowl == 3) {
+        card3.style.backgroundColor = "transparent";
+    } else if (pastBowl == 4) {
+        card4.style.backgroundColor = "transparent";
     }
 }
 
@@ -88,10 +132,7 @@ card1.addEventListener('mouseover', () => {
     foodName1.innerText = "Spicy Soft Tofu";
     foodName2.innerText = "순두부찌개";        
     
-
-    
-    hoverDesign.style.transitionDuration = `${transiLength}s`;
-
+    pastBowl = currBowl;
     currBowl = gotoBowl;
 });
 
@@ -112,6 +153,7 @@ card2.addEventListener('mouseover', () => {
     foodName1.innerText = "Spicy Tteokbokki";
     foodName2.innerText = "떡볶이";
 
+    pastBowl = currBowl;
     currBowl = gotoBowl;
 });
 
@@ -132,6 +174,7 @@ card3.addEventListener('mouseover', () => {
     foodName1.innerText = "Bibimbap";
     foodName2.innerText = "비빔밥";
 
+    pastBowl = currBowl;
     currBowl = gotoBowl;
 });
 
@@ -153,7 +196,7 @@ card4.addEventListener('mouseover', () => {
     foodName1.innerText = "Jajangmyeon";
     foodName2.innerText = "자장면";
     
-
+    pastBowl = currBowl;
     currBowl = gotoBowl;
 });
 
@@ -166,15 +209,18 @@ nextBtn.addEventListener('click', () => {
 });
 
 card1.addEventListener('click', () => {
+    mainMenuCloser();
     foodDescription1.style.zIndex = 3;
     foodDescription1.style.opacity = 1;
     mainMenu.addEventListener('click', () => {
+        
         foodDescription1.style.opacity = 0;
         foodDescription1.style.zIndex = -1;
     });
 });
 
 card2.addEventListener('click', () => {
+    mainMenuCloser();
     foodDescription2.style.zIndex = 3;
     foodDescription2.style.opacity = 1;
     mainMenu.addEventListener('click', () => {
@@ -184,6 +230,7 @@ card2.addEventListener('click', () => {
 });
 
 card3.addEventListener('click', () => {
+    mainMenuCloser();
     foodDescription3.style.zIndex = 3;
     foodDescription3.style.opacity = 1;
     mainMenu.addEventListener('click', () => {
@@ -193,6 +240,7 @@ card3.addEventListener('click', () => {
 });
 
 card4.addEventListener('click', () => {
+    mainMenuCloser();
     foodDescription4.style.zIndex = 3;
     foodDescription4.style.opacity = 1;
     mainMenu.addEventListener('click', () => {
